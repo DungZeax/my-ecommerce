0b1802b9c81add379b992b37187c8de8eb0439f3
dungmt <dungmt10697@gmail.com>
2021-01-25 17:03:00 +0700

    create shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import React from "react";
-import HomePage from "./pages/homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from './pages/shop/shop.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </div>
     );
   }
